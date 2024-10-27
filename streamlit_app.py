1b7a6b01094c386ae1e6e8b68bc02b7eee4506ef
mksyahmi24 <mkhairulsyahmi24@gmail.com>
2024-10-27 11:11:25 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
-
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+st.set_page_config(
+    page_title="Genetic Algorithm"
 )
+
+st.header("Genetic Algorithm", divider="gray")
